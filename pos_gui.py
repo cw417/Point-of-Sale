641da47f1c05e9bb5dd500e1a5250f9d27aac50b
Chris W <chriswhitlock417@gmail.com>
2020-01-08 13:22:33 -0500

    pos_gui.py

--- pos_gui.py
+++ pos_gui.py
@@ -14,20 +14,25 @@ class POS(Frame):
         menu = Menu(self.master)
         self.master.config(menu=menu)
 
+        # File menu
         file = Menu(menu)
-
         file.add_command(label="Exit", command=self.client_exit)
-
         menu.add_cascade(label="File", menu=file)
 
+        # Edit menu
         edit = Menu(menu)
-
         edit.add_command(label="Undo")
-        
         menu.add_cascade(label="Edit", menu=edit)
 
-        exit_button = Button(self, text="Close", command=self.client_exit)
+        # Pages menu
+        pages = Menu(menu)
+        pages.add_command(label="Home")
+        pages.add_command(label="Sales Ledger")
+        pages.add_command(label="Inventory")
+        pages.add_command(label="Totals")
+        menu.add_cascade(label="Pages", menu=pages)
 
+        exit_button = Button(self, text="Exit", command=self.client_exit)
         exit_button.place(x=0, y=0)
 
     def client_exit(self):
