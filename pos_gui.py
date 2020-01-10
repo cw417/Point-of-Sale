cb2c60c3beab61b7b5ef738ff54cda4de639a001
cw417 <chriswhitlock417@gmail.com>
2020-01-10 10:45:12 -0500

    pos_gui.py

--- pos_gui.py
+++ pos_gui.py
@@ -1,46 +1,136 @@
 import tkinter as tk
 
-class POS(tk.Frame):
-    
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.master = master
-        self.init_window()
+LARGE_FONT = ("Verdana", 12)
 
-    def init_window(self):
-        self.master.title("POS")
-        self.pack(fill=tk.BOTH, expand=1)
+class POS(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
 
-        menu = tk.Menu(self.master)
-        self.master.config(menu=menu)
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-        # File menu
-        file = tk.Menu(menu)
-        file.add_command(label="Exit", command=self.client_exit)
-        menu.add_cascade(label="File", menu=file)
+        self.frames = {}
 
-        # Edit menu
-        edit = tk.Menu(menu)
-        edit.add_command(label="Undo")
-        menu.add_cascade(label="Edit", menu=edit)
+        for F in (HomePage, Sales, SalesLedger, Products, Totals):
 
-        # Pages menu
-        pages = tk.Menu(menu)
-        pages.add_command(label="Home")
-        pages.add_command(label="Sales Ledger")
-        pages.add_command(label="Inventory")
-        pages.add_command(label="Totals")
-        menu.add_cascade(label="Pages", menu=pages)
+            frame = F(container, self)
 
-        exit_button = tk.Button(self, text="Exit", command=self.client_exit)
-        exit_button.place(x=0, y=0)
+            self.frames[F] = frame
 
-    def client_exit(self):
-        exit()
+            frame.grid(row=0, column=0, sticky="nsew")
 
-root = tk.Tk()
+        self.frames[F] = frame
+        
+        frame.grid(row=0, column=0, stick="nsew")
+        
+        self.show_frame(HomePage)
+        
+    def show_frame(self, cont):
 
-root.geometry("1200x700")
-app = POS(root)
+        frame = self.frames[cont]
+        frame.tkraise()
 
-root.mainloop()
+class HomePage(tk.Frame):
+
+        def __init__(self, parent, controller):
+            tk.Frame.__init__(self, parent)
+            home_label = tk.Label(self, text="Home Page", font=LARGE_FONT)
+            home_label.grid(row=0, column=0)
+
+            sales_button = tk.Button(self, text="Sales", command=lambda: controller.show_frame(Sales))
+            sales_button.grid(row=1, column=0)
+
+            ledger_button = tk.Button(self, text="Sales Ledger", command=lambda: controller.show_frame(SalesLedger))
+            ledger_button.grid(row=2, column=0)
+
+            products_button = tk.Button(self, text="Products", command=lambda: controller.show_frame(Products))
+            products_button.grid(row=3, column=0)
+
+            totals_button = tk.Button(self, text="Totals", command=lambda: controller.show_frame(Totals))
+            totals_button.grid(row=4, column=0)
+
+class Sales(tk.Frame):
+
+        def __init__(self, parent, controller):
+            tk.Frame.__init__(self, parent)
+            sales_label = tk.Label(self, text="Sales", font=LARGE_FONT)
+            sales_label.grid(row=0, column=0)
+
+            home_button = tk.Button(self, text="Home", command=lambda: controller.show_frame(HomePage))
+            home_button.grid(row=1, column=0)
+
+            ledger_button = tk.Button(self, text="Sales Ledger", command=lambda: controller.show_frame(SalesLedger))
+            ledger_button.grid(row=2, column=0)
+
+            products_button = tk.Button(self, text="Products", command=lambda: controller.show_frame(Products))
+            products_button.grid(row=3, column=0)
+
+            totals_button = tk.Button(self, text="Totals", command=lambda: controller.show_frame(Totals))
+            totals_button.grid(row=4, column=0)
+
+class SalesLedger(tk.Frame):
+
+        def __init__(self, parent, controller):
+            tk.Frame.__init__(self, parent)
+            label = tk.Label(self, text="Sales Ledger", font=LARGE_FONT)
+            label.grid(row=0, column=0)
+
+            home_button = tk.Button(self, text="Home", command=lambda: controller.show_frame(HomePage))
+            home_button.grid(row=1, column=0)
+
+            sales_button = tk.Button(self, text="Sales", command=lambda: controller.show_frame(Sales))
+            sales_button.grid(row=2, column=0)
+
+            products_button = tk.Button(self, text="Products", command=lambda: controller.show_frame(Products))
+            products_button.grid(row=3, column=0)
+
+            totals_button = tk.Button(self, text="Totals", command=lambda: controller.show_frame(Totals))
+            totals_button.grid(row=4, column=0)
+
+class Products(tk.Frame):
+
+        def __init__(self, parent, controller):
+            tk.Frame.__init__(self, parent)
+            label = tk.Label(self, text="Products", font=LARGE_FONT)
+            label.grid(row=0, column=0)
+
+            home_button = tk.Button(self, text="Home", command=lambda: controller.show_frame(HomePage))
+            home_button.grid(row=1, column=0)
+            
+            sales_button = tk.Button(self, text="Sales", command=lambda: controller.show_frame(Sales))
+            sales_button.grid(row=2, column=0)
+
+            ledger_button = tk.Button(self, text="Sales Ledger", command=lambda: controller.show_frame(SalesLedger))
+            ledger_button.grid(row=3, column=0)
+
+            totals_button = tk.Button(self, text="Totals", command=lambda: controller.show_frame(Totals))
+            totals_button.grid(row=4, column=0)
+
+class Totals(tk.Frame):
+
+        def __init__(self, parent, controller):
+            tk.Frame.__init__(self, parent)
+            label = tk.Label(self, text="Totals", font=LARGE_FONT)
+            label.grid(row=0, column=0)
+
+            button = tk.Button(self, text="Home Page", command=lambda: controller.show_frame(HomePage))
+            button.grid(row=1, column=0)
+                        
+            sales_button = tk.Button(self, text="Sales", command=lambda: controller.show_frame(Sales))
+            sales_button.grid(row=2, column=0)
+
+            ledger_button = tk.Button(self, text="Sales Ledger", command=lambda: controller.show_frame(SalesLedger))
+            ledger_button.grid(row=3, column=0)
+
+            products_button = tk.Button(self, text="Products", command=lambda: controller.show_frame(Products))
+            products_button.grid(row=4, column=0)
+
+
+app = POS()
+
+app.geometry("1200x700")
+
+app.mainloop()
+
