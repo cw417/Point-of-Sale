6e8829cfc8b65a62187c5660552300914a575c1c
cw417 <chriswhitlock417@gmail.com>
2020-01-23 13:29:47 -0500

    Added get_total() method to sales_page.py

--- sales_page.py
+++ sales_page.py
@@ -28,42 +28,27 @@ class Sales(tk.Frame):
             tk.Frame.__init__(self, parent)
             self.items = []
             self.prices = []
+            self.current_sale = {}
 
+            # Labels for entry fields
             l_sales = tk.Label(self, text="Sales", font=page_settings.LARGE_FONT)
+            l_item = tk.Label(self, text="Item: ")
+            l_price = tk.Label(self, text="Price: ")
             l_sales.grid(row=0, column=0)
-
-            l_item1 = tk.Label(self, text="Item: ")
-            l_item1.grid(row=2, column=0)
-            l_price1 = tk.Label(self, text="Price: ")
-            l_price1.grid(row=3, column=0)
+            l_item.grid(row=2, column=0)
+            l_price.grid(row=3, column=0)
 
             # Entry fields for item & price
             self.e_item = tk.Entry(self, text="item1")
             self.e_item.grid(row=2, column=1)
             self.e_price = tk.Entry(self, text="price1")
             self.e_price.grid(row=3, column=1)
-            
-            items = []
-            prices = []
-            current_sale = {}
 
-            def get_entries():
-                # Dumps entries to json file to be used by current_sale
-                item = self.e_item.get()
-                items.append(item)
-                price = self.e_price.get()
-                self.items.append(item)
-                self.prices.append(price)
-                print(f"Added items: {item}, ${price}")
-                self.e_item.delete(0, tk.END)
-                self.e_price.delete(0, tk.END)
-
-                
             # Buttons for page functions
             b_add_to_sale = tk.Button(self, text="Add to Sale", command=lambda: get_entries())
-            #b_get_total = tk.Button(self, text="Get Total", command=lambda: )
+            b_get_total = tk.Button(self, text="Get Total", command=lambda: get_total())
             b_add_to_sale.grid(row=10, column=0)
-            #b_get_total.grid(row=11, column=0)
+            b_get_total.grid(row=11, column=0)
 
             # Buttons for page selection
             b_home_page = tk.Button(self, text="Home", command=lambda: controller.show_frame(hp.HomePage))
@@ -74,3 +59,23 @@ class Sales(tk.Frame):
             b_ledger.grid(row=20, column=12)            
             b_products_page.grid(row=20, column=13)
             b_totals_page.grid(row=20, column=14)
+
+            def get_entries():
+                # Dumps entries to json file to be used by current_sale
+                item = self.e_item.get()
+                price = self.e_price.get()
+                self.items.append(item)
+                self.prices.append(price)
+                print(f"Added items: {item}, ${price}")
+                self.e_item.delete(0, tk.END)
+                self.e_price.delete(0, tk.END)
+
+            def get_total():
+                nums = self.prices
+                subtotal = 0
+                for num in nums:
+                    subtotal = subtotal + float(num)
+                total = subtotal * 1.13
+                total = round(total, 2)
+                print(f"Subtotal: ${subtotal}\nTotal: ${total}")
+                return subtotal, total
