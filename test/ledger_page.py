200fab9210dd9d0710f8194c50b8a8264f496a5b
cw417 <chriswhitlock417@gmail.com>
2020-01-29 17:33:19 -0500

    Added method to iterate through nested lists

--- test/ledger_page.py
+++ test/ledger_page.py
@@ -13,17 +13,27 @@ class Ledger(tk.Frame):
             l_ledger = tk.Label(self, text="Sales Ledger", font=page_settings.LARGE_FONT)
             l_ledger.grid(row=0, column=0)
 
+            # Buttons for other pages
             b_home_page = tk.Button(self, text="Home", command=lambda: controller.show_frame(hp.HomePage))
-            b_home_page.grid(row=1, column=0)
-
             b_sales_page = tk.Button(self, text="Sales", command=lambda: controller.show_frame(sp.Sales))
-            b_sales_page.grid(row=2, column=0)
-
             b_products_page = tk.Button(self, text="Products", command=lambda: controller.show_frame(pp.Products))
-            b_products_page.grid(row=3, column=0)
-
             b_totals_page = tk.Button(self, text="Totals", command=lambda: controller.show_frame(tp.Totals))
+            b_home_page.grid(row=1, column=0)
+            b_sales_page.grid(row=2, column=0)
+            b_products_page.grid(row=3, column=0)
             b_totals_page.grid(row=4, column=0)
 
+            # Close button
             b_close = tk.Button(self, text="Close", command=self.quit)
             b_close.grid(row=30, column=0)
+
+            def nest_dict_iter(sale_dict):
+                # Iterates through nested dictionaries to return nested values
+                for k, v in sale_dict.items():
+                    for i in range(0,1):
+                        name = "item_" + str(i)
+                        if name in k:
+                            for t, c in v.items():
+                                print(c)
+            
+            #def dict_list_iter(sale_dict):
